f18a2a1ef4f003240d644948e8cb1bd109bf1fc1
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-03-07 11:14:37 -0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
-cp ./out/control.lua ~/.factorio/scenarios/analysis/control.lua
+factorio_data_dir="$HOME/.factorio"
+save_file="DS MP/2024-03-05_DEFAULT_MP_1-53_29-noPB.zip"
+#save_file=$1
 
-outzip="$HOME/.factorio/saves/DS MP/2024-03-05_DEFAULT_MP_1-53_29-noPB.zip"
-filename=$(basename "$outzip" .zip)
+target_zip="$factorio_data_dir/saves/$save_file"
+echo "target_zip: $target_zip"
+
+if [ ! -f "$target_zip" ]; then
+  echo "File not found!"
+  exit 1
+fi
+
+filename=$(basename "$target_zip" .zip)
 mkdir tmp/"$filename" -p
 cp ./out/control.lua tmp/"$filename"/control.lua
 cd tmp || exit
-zip -u "$outzip" "$filename"/control.lua
+zip -u "$target_zip" "$filename"/control.lua
 cd ..
 rm -r tmp
+echo "Done!"
