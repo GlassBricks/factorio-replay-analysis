d0f25e074f9b13e84358a5d0f1e11b0751ab7f29
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-04-15 21:55:02 -0700

    Add test for player-position

---
+++ test-mod/player-position-test.ts
@@ -0,0 +1,23 @@
+import { testDataCollector } from "./test-util"
+import PlayerPosition from "../src/dataCollectors/player-position"
+import expect from "tstl-expect"
+
+test("player position", () => {
+  const player = game.players[1]
+  const dc = testDataCollector(new PlayerPosition(10))
+  after_ticks(9, () => player.teleport([20, 20]))
+  after_ticks(19, () => player.teleport([10, 50]))
+  after_ticks(29, () => player.teleport([30, 30]))
+  after_ticks(39, () => player.teleport([40, 40]))
+  after_ticks(40, () => {
+    const data = dc.exportData()
+    expect(data.period).toEqual(10)
+    expect(data.players[player.name]).toEqual([
+      [20, 20],
+      [20, 20],
+      [10, 50],
+      [30, 30],
+      [40, 40],
+    ])
+  })
+})
