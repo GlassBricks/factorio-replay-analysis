7b8ea3e268a2df5e87e056f057e65293851b0e7b
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-04-15 20:06:23 -0700

    Track buffer amounts based on majority instead of max

--- src/dataCollectors/buffer-amounts.ts
+++ src/dataCollectors/buffer-amounts.ts
@@ -17,7 +17,7 @@ export interface BufferData {
   buffers: SingleBufferData[]
 }
 
-interface TrackedBufferInfo {
+interface TrackedBufferData {
   item?: string
   name: string
   unitNumber: UnitNumber
@@ -32,10 +32,10 @@ interface TrackedBufferInfo {
   amounts?: [time: number, amount: number][]
 }
 
-export default class BufferAmounts extends EntityTracker<TrackedBufferInfo> implements DataCollector<BufferData> {
+export default class BufferAmounts extends EntityTracker<TrackedBufferData> implements DataCollector<BufferData> {
   constructor(
     public nth_tick_period: number = 60 * 5,
-    public minDataPointsToDetermineItemType: number = 5,
+    public minDataPointsToDetermineItem: number = 5,
   ) {
     super({
       filter: "type",
@@ -43,7 +43,7 @@ export default class BufferAmounts extends EntityTracker<TrackedBufferInfo> impl
     })
   }
 
-  protected override initialData(entity: LuaEntity): TrackedBufferInfo | undefined {
+  protected override initialData(entity: LuaEntity): TrackedBufferData | undefined {
     if (!entity.get_inventory(defines.inventory.chest)) return undefined
     return {
       name: entity.name,
@@ -54,51 +54,48 @@ export default class BufferAmounts extends EntityTracker<TrackedBufferInfo> impl
     }
   }
 
-  // returns undefined if tied
-  private getMaxKey(obj: Record<string, number>): string | undefined {
+  private getMajorityKey(obj: Record<string, number>): string | undefined {
     let maxKey: string | undefined
     let max = 0
+    let total = 0
     for (const [key, value] of pairs(obj)) {
-      if (value == max) {
-        maxKey = undefined
-      }
       if (value > max) {
         max = value
         maxKey = key
       }
+      total += value
     }
-    return maxKey
+    if (max > total / 2) return maxKey
   }
 
-  protected override onPeriodicUpdate(entity: LuaEntity, data: TrackedBufferInfo) {
+  protected override onPeriodicUpdate(entity: LuaEntity, data: TrackedBufferData) {
     const amounts = data.amounts
     if (amounts) {
       const itemCount = entity.get_inventory(defines.inventory.chest)!.get_item_count(assert(data.item))
       amounts.push([game.tick, itemCount])
     } else {
       const itemCounts = assert(data.itemCounts)
-      // still trying to determine what the dominant item is
       const counts = entity.get_inventory(defines.inventory.chest)!.get_contents()
       if (next(counts)[0] == undefined) return
       itemCounts.push({ time: game.tick, counts })
-      if (itemCounts.length == this.minDataPointsToDetermineItemType) {
+      if (itemCounts.length == this.minDataPointsToDetermineItem) {
         this.determineItemType(data)
       }
       return
     }
   }
 
-  private determineItemType(data: TrackedBufferInfo) {
+  private determineItemType(data: TrackedBufferData) {
     const maxAtTime: Record<string, number> = {}
     const itemCounts = data.itemCounts!
     for (const { counts } of itemCounts) {
-      const maxKey = this.getMaxKey(counts)
+      const maxKey = this.getMajorityKey(counts)
       if (maxKey) {
         maxAtTime[maxKey] = (maxAtTime[maxKey] ?? 0) + 1
       }
     }
-    const finalMax = this.getMaxKey(maxAtTime)
-    if (!(finalMax && maxAtTime[finalMax] > this.minDataPointsToDetermineItemType / 2)) {
+    const finalMax = this.getMajorityKey(maxAtTime)
+    if (!(finalMax && maxAtTime[finalMax] > this.minDataPointsToDetermineItem / 2)) {
       // a multiplicity of items, probably not a buffer
       this.removeEntry(data.unitNumber)
       return
